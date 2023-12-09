df355b5eeb718dedac66f35c1d658589572254e5
mesteranas <135442710+mesteranas@users.noreply.github.com>
2023-12-09 15:09:30 +0200

    09/12/2023

--- anas main/main.py
+++ anas main/main.py
@@ -6,7 +6,7 @@ import guiTools
 from settings import *
 import PyQt6.QtWidgets as qt
 import PyQt6.QtGui as qt1
-from PyQt6.QtCore import Qt
+import PyQt6.QtCore as qt2
 language.init_translation()
 class main (qt.QMainWindow):
     def __init__(self):
